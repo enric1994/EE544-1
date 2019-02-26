ff5b40f9dc9a724baea93e33ab33eb0762ac8717
Enric Moreu <enric.moreu.filella@gmail.com>
2019-02-26 16:38:49 +0000

    transfer learning using keras.io example very slow

---
+++ src/model/transfer.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# (c) Copyright 2019 Enric Moreu. All Rights Reserved.
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Model
+from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, GlobalAveragePooling2D, Dropout, Flatten
+from keras import callbacks
+from keras import optimizers
+from keras.applications import InceptionV3
+
+#Remove warnings????
+# import os
+# import tensorflow as tf
+
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  
+
+#Not working with pretrained model
+# import keras.backend as K
+# K.set_floatx('float16')
+
+experiment = '3.0.12'
+
+train_path = '/data/resized_299/train'
+validation_path = '/data/resized_299/validation'
+image_size = 299
+epochs = 50
+batch_size = 32 #32 works too
+steps_per_epoch = 2000
+validation_steps = 800
+
+#Load data + augmentation
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+val_datagen = ImageDataGenerator(
+        rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_path,
+        target_size=(image_size, image_size),
+        batch_size=batch_size,
+        class_mode='binary') 
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+validation_generator = val_datagen.flow_from_directory(
+        validation_path,
+        target_size=(image_size, image_size),
+        batch_size=batch_size,
+        class_mode='binary') 
+
+
+# Define model
+base_model = InceptionV3(weights='imagenet', include_top=False)
+
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(128, activation='relu')(x)
+predictions = Dense(1, activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
+
+for i, layer in enumerate(base_model.layers):
+   print(i, layer.name)
+
+# we chose to train the top 2 inception blocks, i.e. we will freeze
+# the first 249 layers and unfreeze the rest:
+for layer in model.layers[:249]:
+   layer.trainable = False
+for layer in model.layers[249:]:
+   layer.trainable = True
+print(model.summary())
+# Define optimizer
+sgd = optimizers.SGD(lr=0.0001, decay=1e-6, momentum=0.9, nesterov=True)
+
+model.compile(loss = 'binary_crossentropy',
+              optimizer = 'rmsprop',
+              metrics = ['accuracy'])
+
+# Tensorboard
+tbCallBack = callbacks.TensorBoard(log_dir='/code/logs/{}'.format(experiment))
+
+model.fit_generator(
+        train_generator,
+        steps_per_epoch=steps_per_epoch // batch_size,
+        epochs=epochs,
+        validation_data=validation_generator,
+        validation_steps=validation_steps // batch_size,
+        callbacks=[tbCallBack])
