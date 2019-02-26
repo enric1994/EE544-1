1108fa3cd179fc70618eb5a96e3cf2270517f688
Enric Moreu <enric.moreu.filella@gmail.com>
2019-02-26 19:22:54 +0000

    heavy data augmentation and reduce lr

--- src/model/transfer.py
+++ src/model/transfer.py
@@ -10,34 +10,49 @@ from keras import optimizers
 from keras.applications import InceptionV3
 
 #Remove warnings????
-# import os
-# import tensorflow as tf
+import os
+import tensorflow as tf
 
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  
 
 #Not working with pretrained model
 # import keras.backend as K
 # K.set_floatx('float16')
 
-experiment = '3.0.18'
+experiment = '3.1.13'
 
 train_path = '/data/resized_299/train'
 validation_path = '/data/resized_299/validation'
 image_size = 299
-epochs = 50
-batch_size = 32 #32 works too
-steps_per_epoch = 500
-validation_steps = 200
+epochs = 500
+batch_size = 32
+steps_per_epoch = 1600
+validation_steps = 400
 
 #Load data + augmentation
 train_datagen = ImageDataGenerator(
         rescale=1./255,
-        shear_range=0.2,
         zoom_range=0.2,
-        horizontal_flip=True)
+        # shear_range=0.2,
+        brightness_range=(0.5,1.5),
+        # zca_whitening=True,
+        # featurewise_std_normalization=True,
+        horizontal_flip=True,
+        rotation_range=15,
+        width_shift_range=.15,
+        height_shift_range=.15)
 
 val_datagen = ImageDataGenerator(
-        rescale=1./255)
+        rescale=1./255,
+        zoom_range=0.2,
+        # shear_range=0.2,
+        brightness_range=(0.5,1.5),
+        # zca_whitening=True,
+        # featurewise_std_normalization=True,
+        horizontal_flip=True,
+        rotation_range=15,
+        width_shift_range=.15,
+        height_shift_range=.15)
 
 train_generator = train_datagen.flow_from_directory(
         train_path,
@@ -58,7 +73,10 @@ base_model = InceptionV3(weights='imagenet', include_top=False)
 
 x = base_model.output
 x = GlobalAveragePooling2D()(x)
-# x = Dense(12, activation='relu')(x)
+# x = Dropout(0.5)(x)
+# x = Flatten(input_shape=(2048,))(x)
+# x = Dense(64, activation='relu')(x)
+x = Dropout(0.5)(x)
 predictions = Dense(1, activation='sigmoid')(x)
 model = Model(inputs=base_model.input, outputs=predictions)
 
@@ -68,21 +86,27 @@ for layer in model.layers[:249]:
    layer.trainable = False
 for layer in model.layers[249:]:
    layer.trainable = True
-print(model.summary())
+# print(model.summary())
+
 # Define optimizer
-sgd = optimizers.SGD(lr=0.001, decay=1e-6, momentum=0.9, nesterov=True)
+sgd = optimizers.SGD(lr=0.000001, decay=1e-6, momentum=0.9, nesterov=True)
 
 model.compile(loss = 'binary_crossentropy',
-              optimizer = 'sgd',
+              optimizer = sgd,
               metrics = ['accuracy'])
 
 # Tensorboard
 tbCallBack = callbacks.TensorBoard(log_dir='/code/logs/{}'.format(experiment))
 
+# LR scheduler
+reduce_lr = callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5,
+                              patience=10, min_lr=0.0000000001, verbose=1)
+
 model.fit_generator(
         train_generator,
-        steps_per_epoch=steps_per_epoch // batch_size,
+        steps_per_epoch=steps_per_epoch // batch_size, #total images/bs (modify names)
         epochs=epochs,
         validation_data=validation_generator,
-        validation_steps=validation_steps // batch_size,
-        callbacks=[tbCallBack])
+        validation_steps=validation_steps // batch_size, #total images/bs
+        callbacks=[tbCallBack, reduce_lr],
+        shuffle=True)
