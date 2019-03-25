e9365cccd9f70bd858d31e715c097ccf351f9649
Enric Moreu <enric.moreu.filella@gmail.com>
2019-03-25 21:33:39 +0000

    lr_find

--- src/model/basic.py
+++ src/model/basic.py
@@ -8,17 +8,18 @@ from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Dropout, Flatt
 from keras import callbacks
 from keras import optimizers
 
-import keras.backend as K
-K.set_floatx('float16')
 
-experiment = '1.2.4'
+# import keras.backend as K
+# K.set_floatx('float16')
+
+experiment = '1.2.8'
 
 train_path = '/data/resized_224/train'
 validation_path = '/data/resized_224/validation'
 epochs = 100
-batch_size = 128
-steps_per_epoch = 2000
-validation_steps = 800
+batch_size = 32
+steps_per_epoch = 1600
+validation_steps = 384
 
 #Load data + augmentation
 train_datagen = ImageDataGenerator(
@@ -36,12 +37,7 @@ train_generator = train_datagen.flow_from_directory(
         class_mode='binary') 
 
 validation_datagen = ImageDataGenerator(
-        rescale=1./255,
-        zoom_range=0.2,
-        horizontal_flip=True,
-        rotation_range=15,
-        width_shift_range=.15,
-        height_shift_range=.15)
+        rescale=1./255)
 
 validation_generator = train_datagen.flow_from_directory(
         validation_path,
@@ -70,8 +66,8 @@ model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
 # Define optimizer
-# opt = optimizers.SGD(lr=1e-4, decay=0, momentum=0.8, nesterov=False)
-opt = optimizers.Adam(lr=1e-5, decay=1e-5 / epochs)
+opt = optimizers.SGD(lr=1e-1, decay=0, momentum=0.9, nesterov=False)
+# opt = optimizers.Adam(lr=1e-2)
 
 model.compile(loss = 'binary_crossentropy',
               optimizer = opt,
@@ -88,10 +84,18 @@ tbCallBack = callbacks.TensorBoard(log_dir='/code/logs/{}'.format(experiment))
 checkpoints = callbacks.ModelCheckpoint('/code/checkpoints/{}.weights'.format(experiment), monitor='val_acc', verbose=2, save_best_only=True, save_weights_only=False, mode='auto', period=1)
 
 
+from utils.clr import LRFinder
+lr_finder = LRFinder(1600, batch_size, minimum_lr=1e-6, maximum_lr=10.,
+                     lr_scale='exp',
+                     save_dir='/code/docs/plots/lr_finder', verbose=True)
+epochs = 1
+
 model.fit_generator(
         train_generator,
         steps_per_epoch=steps_per_epoch // batch_size,
         epochs=epochs,
         validation_data=validation_generator,
         validation_steps=validation_steps // batch_size,
-        callbacks=[tbCallBack, checkpoints])
+        callbacks=[tbCallBack, checkpoints, lr_finder])
+
+lr_finder.plot_schedule_from_file('/code/docs/plots/lr_finder',experiment)
