f40bff315be246d1e997048271f2bc1ec377bba6
Enric Moreu <enric.moreu.filella@gmail.com>
2019-03-29 14:12:15 +0000

    model 3.2.12 lr 1e-4 alpha 0.001 dropout 0.5 batch norm only in last layers

--- src/model/transfer.py
+++ src/model/transfer.py
@@ -21,17 +21,17 @@ import os
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  
 
-experiment = '3.2.10'
+experiment = '3.2.12'
 
 train_path = '/data/resized_299/train'
 validation_path = '/data/resized_299/validation'
 test_path = '/data/resized_299/test'
 epochs = 150
 batch_size = 64
-lr = 1e-6
+lr = 1e-4
 decay = 0
 max_lr=1e-1
-alpha = 0.0005
+alpha = 0.001
 
 
 
@@ -81,7 +81,7 @@ x = base_model.output
 x = GlobalAveragePooling2D()(x)
 # x = BatchNormalization()(x)
 # x = Activation('relu')(x)
-x = Dropout(0.2)(x)
+x = Dropout(0.5)(x)
 predictions = Dense(1, activation='sigmoid')(x)
 model = Model(inputs=base_model.input, outputs=predictions)
 
@@ -89,10 +89,12 @@ for layer in model.layers[:249]:
    layer.trainable = False
 for layer in model.layers[249:]:
    layer.trainable = True
-
-for layer in model.layers:
-    if isinstance(layer, Conv2D):
+   if isinstance(layer, Conv2D):
         layer.add_loss(regularizers.l2(alpha)(layer.kernel))
+
+# for layer in model.layers:
+#     if isinstance(layer, Conv2D):
+#         layer.add_loss(regularizers.l2(alpha)(layer.kernel))
 
 # Define optimizer
 opt = optimizers.Adam(lr=lr, decay=decay)
