bf29a119de9819a9e0f2b9f30c297b259a95803d
Enric Moreu <enric.moreu.filella@gmail.com>
2019-04-08 13:20:13 +0100

    binary metrics + no shuffle

--- src/utils/metrics.py
+++ src/utils/metrics.py
@@ -5,6 +5,10 @@ from keras.models import Model, load_model
 from sklearn import metrics
 import numpy as np
 
+import keras.backend as K
+K.set_floatx('float16')
+
+
 def get_metrics(experiment,image_size):
     test_path = '/data/resized_{}/test'.format(image_size)
     batch_size = 64
@@ -14,28 +18,26 @@ def get_metrics(experiment,image_size):
 
     test_generator = test_datagen.flow_from_directory(
             test_path,
-            target_size=(299, 299),
+            target_size=(image_size, image_size),
             batch_size=batch_size,
-            class_mode='binary')
+            class_mode='binary',
+            shuffle = False)
 
 
     model = load_model('/code/checkpoints/{}.weights'.format(experiment))
 
     predictions = model.predict_generator(test_generator)
 
-    val_preds = (predictions < 0.5).astype(np.int)
+    val_preds = (predictions >= 0.5).astype(np.int)
     val_trues = test_generator.classes
     cm = metrics.confusion_matrix(val_trues, val_preds)
 
     labels = test_generator.class_indices.keys()
-    precisions, recall, f1_score, _ = metrics.precision_recall_fscore_support(val_trues, val_preds, labels=[0,1])
-    print('Precision for "No Finding": {}'.format(precisions[0]))
-    print('Precision for "Cardiomegaly": {}'.format(precisions[1]))
-    print('Recall for "No Finding": {}'.format(recall[0]))
-    print('Recall for "Cardiomegaly": {}'.format(recall[1]))
-    print('F1 score for "No Finding": {}'.format(f1_score[0]))
-    print('F1 score for "Cardiomegaly": {}'.format(f1_score[1]))
+    print('Accuracy: {}'.format(metrics.accuracy_score(val_trues, val_preds)))
+    print('Precision: {}'.format(metrics.precision_score(val_trues, val_preds)))
+    print('Recall: {}'.format(metrics.recall_score(val_trues, val_preds)))
+    print('F1 score: {}'.format(metrics.f1_score(val_trues, val_preds)))
     print('Confusion matrix:')
     print(cm)
 
-get_metrics('3.14.2',299)
+get_metrics('1.4.23',224)
